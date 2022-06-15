9eedfb36a7ae9422b27e800717204c864616a898
dizzysoup <j481936@gamil.com>
2022-06-15 21:55:31 +0800

    CPE01

---
+++ Vito'sFamily/uva10041.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import static java.lang.System.*;
+public class uva10041{
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+ 	int cases = sc.nextInt();
+ 	while((--cases) >=0){
+ 		int size = sc.nextInt();
+ 		int[] arr = new int[size];
+ 		for(int i = 0 ; i< size ; i++)
+ 			arr[i] = sc.nextInt(); 		
+ 		Arrays.sort(arr); 		
+ 		int len = 0 ;
+ 		for(int i = 0 ; i<size ; i++){
+ 			len += Math.abs(arr[i] - arr[i/2]); 		
+        }	
+ 		System.out.println(len);
+ 	}
+  }
+};
