11fb262ea0af7aa20be3096d743ef6640c9d5446
dizzysoup <j481936@gamil.com>
2022-06-15 22:23:17 +0800

    cpe02

---
+++ Hashmat the Brave Warrior/uva10055.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import static java.lang.System.*;
+public class uva10055{
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+  	while(sc.hasNextLong()){
+  		long n = sc.nextLong();
+  		long m = sc.nextLong();
+  		out.println(Math.abs(n-m));
+  	}
+  }
+};
+ 
+       
+ 
+         
+ 
+  
+ 
+  
+ 
+   
+
+
+ 
